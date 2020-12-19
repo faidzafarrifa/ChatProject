aacdeda371a593394067cf4b5ac094d8caf06fee
faidzafarrifa <faidzafarrifa@gmail.com>
2020-12-19 22:23:29 +0700

    LoginRegister

---
+++ app/src/main/java/com/example/chatproject/SignIn.java
@@ -0,0 +1,16 @@
+package com.example.chatproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chatproject.R;
+
+public class SignIn extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in);
+    }
+}
+
